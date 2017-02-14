701626c8f4d256e3ddeccd838c64f57a20eac6ad
Sierra <sierramroberts.96@gmail.com>
2017-02-14 15:10:11 -0500

    Tested

---
+++ js/flow-control.js
@@ -0,0 +1,133 @@
+// var todayIs ='Monday';
+
+
+// // if ('2' === 2) {
+// //     console.log('Basically the same.');
+// // }
+// // else {
+// //     console.log('So different.');
+// // }
+
+// if (todayIs === 'Tuesday') { 
+//     console.log('Happy Valentines Day!');
+// }
+// else if (todayIs === 'Wednesday') {
+//     console.log('Happy Humpday!');
+
+// }
+// else { console.log('Happy ${todayIs}!');
+// }
+
+// if (todayIs === 'Wednesday') { 
+//     console.log('Happy Humpday!');
+// }
+// else { 
+//     console.log('Happy ${todayIs}!');
+// }
+// // else { 
+// //     console.log('Happy ' + todayIs + '!');
+// // }
+// // you can use this too, just a different way. 
+
+
+// **** Case / Module Math ? ****
+
+// var todayIsTuesday = true;  
+// // console.log(typeof todayIsTuesday);
+
+
+// var counter = 0;
+// // counter++;
+// counter = counter + 2;
+// // counter += 2;
+// // console.log(counter);
+
+// if (counter > 2) {
+//     console.log('Correct answer.');
+// }
+
+// if (typeof todayIsTuesday === 'boolean') {
+
+// }
+
+
+
+
+// **** Case ****
+
+// var slicesOfPizza = 12;
+
+// if (slicesOfPizza % 3 === 0 || slicesOfPizza % 4 === 0 || slicesOfPizza % 5 === 0) {
+//     console.log('There\'s enough for both of you.');
+// }
+// else if (slicesOfPizza % 3 === 0) { 
+//     console.log('Enough for the three of you.');
+// }
+// else {
+//     console.log('Not going to divide evenly.');
+// }
+
+
+
+// ***** Switch *****
+
+// var temperature = 50;
+
+
+// switch (temperature) {
+//     case 50:
+//         console.log('Getting warmer.');
+//         break; 
+//     case 60: 
+//         console.log('Getting closer to Spring.');
+//         break;
+//     case 70: 
+//         console.log('Spring is here.');
+//         break;
+//     default:  
+//         console.log('Who knows. Welcome to Indiana.');
+// }
+
+
+// *****teranary operators *****
+// var favoriteColor = 'red';
+
+// var message = favoriteColor === 'red' ? 'My favorite color is red.' : 'My favorite color is not red.';
+
+// console.log(message);
+
+
+// **** Loooop ****
+
+// for (var counter = 0; counter < 500000; counter += 500) {
+//     console.log(counter);
+// }
+
+
+// for (var i = 0; i < 20; i++) { 
+//     document.getElementById('robots').innerHTML += '<img src="https://robohash.org/indy"/>';
+// }
+
+
+// function addOne(number) {
+//     return number + 1;
+// }
+
+
+// var result = addOne(100);
+
+// console.log(result);
+
+
+
+// function capitalize(word,foo) { 
+
+    
+// }
+
+
+
+function FizzBuzz(n) { 
+    var output = '';
+    return output;
+}
